782c2b11ad7d3edf8bbbf92ac78c7afdb8b7027b
fantom2513 <fantom2513@yandex.ru>
2023-05-11 21:22:47 +0300

    m15

---
+++ module15/chat.cpp
@@ -0,0 +1,35 @@
+#include <cstring>
+#include "chat.h"
+#include "hash.h"
+#include "sha1.h"
+
+Chat::Chat()
+{
+	_data = new HashTable();
+}
+
+Chat::~Chat()
+{
+	delete _data;
+}
+
+void Chat::reg(const char _login[LOGINLENGTH], const char _pass[], const int pass_length) {
+	// Ваш код
+	const uint* hash = sha1(_pass, pass_length);
+
+	_data->add(_login, hash);
+	delete[] hash;
+}
+bool Chat::login(const char _login[LOGINLENGTH], const char _pass[], const int pass_length) {
+	// Ваш код
+	// Нужно вернуть true в случае успешного логина
+	const uint* saved = _data->find(_login);
+	if (saved)
+	{
+		const uint* current = sha1(_pass, pass_length);
+		bool logged = !memcmp(saved, current, SHA1HASHLENGTHBYTES);
+		delete[]current;
+		return logged;
+	}
+	return false;
+}
---
+++ module15/chat.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <cstring>
+#include "sha1.h"
+#include "hash.h"
+
+#define LOGINLENGTH 10
+
+class Chat {
+public:
+    Chat();
+    ~Chat();
+    void reg(const char _login[LOGINLENGTH], const char _pass[], const int pass_length);
+    bool login(const char _login[LOGINLENGTH], const char _pass[], const int pass_length);
+
+private:
+    HashTable* _data;
+};
