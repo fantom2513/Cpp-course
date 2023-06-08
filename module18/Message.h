32db332957d96ecab56018e73c0aa53dead602f3
fantom2513 <fantom2513@yandex.ru>
2023-06-08 21:57:41 +0300

    hw
    use script to run the programm

---
+++ module18/Message.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <fstream>
+
+class Message {
+	std::string _text;
+	std::string _sender;
+	std::string _receiver;
+public:
+	Message(std::string text, std::string sender, std::string receiver) :_text(text), _sender(sender), _receiver(receiver) {}
+
+	friend std::fstream& operator >>(std::fstream& inStream, Message& message);
+	friend std::ostream& operator <<(std::ostream& outStream, const Message& message);
+};
---
+++ module18/User.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <fstream>
+
+class User {
+	std::string _name;
+	std::string _login;
+	std::string _pass;
+
+public:
+	User(std::string name, std::string login, std::string pass) :_name(name), _login(login), _pass(pass) {}
+
+	friend std::fstream& operator >>(std::fstream& inStream, User& user);
+	friend std::ostream& operator <<(std::ostream& outStream, const User& user);
+};
+
